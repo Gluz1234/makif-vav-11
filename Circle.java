9db7154bc3531deb3513feba17622cfc0576c462
Gluz1234 <137111903+Gluz1234@users.noreply.github.com>
2023-12-31 14:42:43 +0200

    Updated Circle.java

--- Circle.java
+++ Circle.java
@@ -38,4 +38,37 @@ public class Circle {
         System.out.println("centerX = " + _center.get_x());
         System.out.println("centerY = " + _center.get_y());
     }
+
+    public double perimeter()
+    {
+        return 2*3.14*_radius;
+    }
+    public double area()
+    {
+        return 3.14*_radius*_radius;
+    }
+    public void move(int deltaX,int deltaY)
+    {
+        _center.move(deltaX,deltaY);
+    }
+    public boolean equals(Circle other)
+    {
+        boolean xCheck = _center.get_x() == other.get_center().get_x();
+        boolean yCheck = _center.get_y() == other.get_center().get_y();
+        boolean radiusCheck = _radius == other.get_radius();
+        return xCheck && yCheck && radiusCheck;
+    }
+    public boolean isLarger(Circle other)
+    {
+        return (this.area() > other.area());
+    }
+
+    public boolean isInCircle(Point p)
+    {
+        return ((Math.pow(p.get_x() - _center.get_x(), 2) + Math.pow(p.get_y() - _center.get_y(), 2)) < Math.pow(_radius, 2));
+    }
+    public String toString()
+    {
+        return "The center is:(" + _center.get_x() + "," + _center.get_y() + ") and the radius = " + _radius;
+    }
 }
