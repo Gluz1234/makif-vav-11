b292be544dd0fcffae0872497a3249277889d03a
Alon Gluzman <alondg11@gmail.com>
2024-01-12 17:20:06 +0200

    did the first question

--- tests/test1/src/Main.java
+++ tests/test1/src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static boolean ex1a(String str, int len)
+    {
+        return str.length()>len;
+    }
+    public static int ex1b(String[] arr,int num)
+    {
+        int count = 0;
+        for (String word : arr)
+        {
+            if (ex1a(word, num))
+            {
+                count++;
+            }
         }
+        return count;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(ex1b(new String[]{"aba","saba","dana","hana","ana"},3));
     }
 }
