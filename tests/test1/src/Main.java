c3c15bf110dda5dbc913f7af86f8cf90f979dd3a
Alon Gluzman <alondg11@gmail.com>
2024-01-12 17:42:53 +0200

    did the forth question

--- tests/test1/src/Main.java
+++ tests/test1/src/Main.java
@@ -15,8 +15,20 @@ public class Main {
         }
         return count;
     }
+    public static int ex4(int[] arr)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i] % 2 != 0 && arr[i+1] % 2 != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args)
     {
         System.out.println(ex1b(new String[]{"aba","saba","dana","hana","ana"},3));
+        System.out.println(ex4(new int[]{2,-2,3,7,-5,4,8}));
     }
 }
